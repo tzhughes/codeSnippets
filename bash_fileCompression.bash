44407ec585401bbf78cbe82ec1a8a2eec543ab0a
Tim Hughes <tim@pcus373.uio.no>
2015-06-08 14:15:48 +0200

    Adding compression commands

--- bash_fileCompression.bash
+++ bash_fileCompression.bash
@@ -1,4 +1,15 @@
 # File decompression commands
 
-# ZIP
-# - zip leaves a copyt of the original archive by defaults
+## ZIP
+# zip leaves copy of original archive by default, no need to do anything specific
+# zip will prompt user if inflating will cause overwriting of existing file, so need to force it with -o
+# zip will decompress to pwd, so need to provide the destination directory (which is dir of file to be decompressed)
+unzip -o ${destFile} -d `dirname ${destFile}` 
+fi
+
+## GZIP
+# gzip will not leave copy of original archive, thus the -c option
+# We explicitly specify output file, solving overwriting and location issues
+gunzip -c ${destFile} > ${destFile%.*} 
+fi
+}
