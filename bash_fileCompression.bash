b71fd725105f80706ac5539e15a9122e2e6e8d23
Tim Hughes <tim@pcus373.uio.no>
2015-06-08 14:16:37 +0200

    Type fix

--- bash_fileCompression.bash
+++ bash_fileCompression.bash
@@ -5,11 +5,8 @@
 # zip will prompt user if inflating will cause overwriting of existing file, so need to force it with -o
 # zip will decompress to pwd, so need to provide the destination directory (which is dir of file to be decompressed)
 unzip -o ${destFile} -d `dirname ${destFile}` 
-fi
 
 ## GZIP
 # gzip will not leave copy of original archive, thus the -c option
 # We explicitly specify output file, solving overwriting and location issues
 gunzip -c ${destFile} > ${destFile%.*} 
-fi
-}
